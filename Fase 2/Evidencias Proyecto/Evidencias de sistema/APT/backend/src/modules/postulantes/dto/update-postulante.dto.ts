3a6ccff62cc7f102c7580b3299e3efc9dbb8f7db
Sebitax <sebigonza1701@gmail.com>
2025-11-06 12:58:22 -0300

    Add initial documentation files for project evidence in Phase 2

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/APT/backend/src/modules/postulantes/dto/update-postulante.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePostulanteDto } from './create-postulante.dto';
-
-export class UpdatePostulanteDto extends PartialType(CreatePostulanteDto) {}
